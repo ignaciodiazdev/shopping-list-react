7cf391c0cb8a80d00694be0932094b840a54b0cd
ignaciodz <ignaciodiazbr@gmail.com>
2025-03-14 22:02:50 -0500

    add: Agregar componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="py-6 bg-green-600">
+      <h1 className="text-3xl text-center text-white font-black">
+        Shopping List - IgnacioDiaz
+      </h1>
+    </header>
+  );
+}
