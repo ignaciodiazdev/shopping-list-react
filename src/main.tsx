cb77453d2b13ed40e3a105b9ed613532c4d17a79
ignaciodz <ignaciodiazbr@gmail.com>
2025-03-15 22:12:43 -0500

    add: Agregar css de SweetAlert2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <>
